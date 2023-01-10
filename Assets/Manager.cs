cdbd288e6ce5b9547bab1e72b4edd10453696867
Sergey <sergey.sergeev.v.99@gmail.com>
2023-01-10 20:06:09 +0300

    init

---
+++ Assets/Manager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+   public void RotateGameObject(GameObject go, float x, float y, float z) 
+   {
+        /*
+         Многа кода
+         */
+        go.GetComponent<Transform>().Rotate(x, y, z, Space.World);
+   }
+
+    private void Update()
+    {
+        /*
+         Многа кода
+         */
+    }
+}
---
+++ Assets/Ratator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ratator : MonoBehaviour
+{
+    public float Speed = 1;
+    public Vector3 angle;
+    Manager mng = null;
+    private void Awake()
+    {
+        Debug.Log("Awake");
+        mng = FindObjectOfType<Manager>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GetComponent<Transform>().Rotate(Vector3.up);
+        GetComponent<Transform>().Rotate(angle * Speed, Space.World);
+
+        
+        if (Input.GetButtonDown("Space"))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    private void LateUpdate()
+    {
+        
+    }
+    private void OnEnable()
+    {
+        
+    }
+    private void OnDisable()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        //gameObject.SetActive(false);
+        Destroy(gameObject);
+    }
+    private void OnDestroy()
+    {
+        Debug.Log("Destroying");
+    }
+}
